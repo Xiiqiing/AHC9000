fb3f5adf5dd22b66df3ba3e5effedec0cd141cf4
XQ <873960774mxq@gmail.com>
2025-08-08 12:30:52 +0200

    Upload new files

--- components/sentio/climate/sentio_climate.cpp
+++ components/sentio/climate/sentio_climate.cpp
@@ -35,8 +35,7 @@ void SentioClimate::control(const climate::ClimateCall& call) {
     this->target_temperature = *call.get_target_temperature();
     float target = target_temperature;
     ESP_LOGD(TAG, "Target temperature changed to: %f", target);
-    //temp_setpoint_number_->set(target);
-    temp_setpoint_number_->make_call().set_value(target).perform();//set(target);
+    temp_setpoint_number_->set(target);
   }
 }
 
@@ -44,7 +43,7 @@ climate::ClimateTraits SentioClimate::traits() {
   auto traits = climate::ClimateTraits();
 
   traits.set_supports_current_temperature(true);
-  traits.set_visual_temperature_step(0.1);
+  traits.set_visual_temperature_step(0.5);
   traits.set_visual_min_temperature(5);
   traits.set_visual_max_temperature(30);
 
